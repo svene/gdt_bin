1691acf5aeebafa1ec1d3c092d7fe7b1dfb1258b
Sven Ehrke <sven.ehrke@canoo.com>
2013-03-31 23:01:44 +0200

    0.0.9

--- gdt.groovy
+++ gdt.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='sven-github', root='http://svene.github.com/maven2/releases')
-@Grab(group='org.svenehrke', module='directory_template', version='0.0.8')
+@Grab(group='org.svenehrke', module='directory_template', version='0.0.9')
 import org.svenehrke.directorytemplate.tool.GdtMain
 
 new GdtMain().run(args)
