5f58d992830318416b4f68552bc3f64a12bb792b
Sven Ehrke <sven.ehrke@canoo.com>
2013-04-20 19:36:52 +0200

    fixed bug in starting selfupdate on windows

--- gdt.groovy
+++ gdt.groovy
@@ -2,10 +2,11 @@
 @Grab(group='org.svenehrke', module='directory_template', version='0.0.14')
 import org.svenehrke.directorytemplate.tool.GdtMain
 
+
 if (args.size() == 1 && args[0] == 'selfupdate') {
-	File f = new File("${getClass().protectionDomain.codeSource.location.path}")
-	String scriptFolder = f.parentFile.absolutePath
-	run (new File("$scriptFolder/gdt_update.groovy"), [] as String[])
+	String userHome = System.properties['user.home']
+	String gdtHome = "${userHome}/.gdt"
+	run (new File("$gdtHome/bin/gdt_update.groovy"), [] as String[])
 }
 else {
 	new GdtMain().run(args)
